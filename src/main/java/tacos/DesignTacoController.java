88d8276abf1aaf0f175f2fbdc7f09a0a55f0b1cc
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-10-26 10:32:20 -0400

    completed up to 2.3.3 Displaying Validation Errors

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,9 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
-
-import tacos.Taco;
-import tacos.Ingredient;
 import tacos.Ingredient.Type;
 
 @Slf4j
@@ -49,11 +48,13 @@ public class DesignTacoController {
     }
     
     @PostMapping
-    public String processDesign(Taco design) {
-//        System.out.println("Processing design: " + design);
+    public String processDesign(@Valid Taco design, Errors errors) {
+        
+        if(errors.hasErrors()) return "design";
+        
         log.info("Processing design: " + design);
         
-        return "redirect:/orders/current";
+        return "redirect:orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
