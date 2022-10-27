263f347156a848f0b01e8644a006824300d2a393
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-10-27 14:22:21 -0400

    finished 3.1

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -8,27 +8,34 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.domain.Order;
+import tacos.repository.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
     
+    private OrderRepository orderRepo;
+    
+    public OrderController(OrderRepository orderRepo) {this.orderRepo = orderRepo;}
+    
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
-        return "orderForm";
-    }
+    public String orderForm(Model model) {return "orderForm";}
     
     @PostMapping
-    public String processOrder(@Valid Order order, Errors error) {
+    public String processOrder(@Valid Order order, SessionStatus sessionStatus, Errors error) {
         
         if(error.hasErrors()) return "orderForm";
         
-        log.info("Order Submiteed: " + order);
+        orderRepo.save(order);
+
+        sessionStatus.setComplete();
         
         return "redirect:/";
     }
