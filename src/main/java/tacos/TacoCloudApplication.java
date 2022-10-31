161d2a5847c765f8ecd0bad70944a4c0ab7af8a2
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-10-31 16:54:07 -0400

    no code section

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// Chapter 5: Working with Configuration Properties
+// Chapter 5.1: Fine-Tuning Autoconfiguration
 
 @SpringBootApplication
 public class TacoCloudApplication {
