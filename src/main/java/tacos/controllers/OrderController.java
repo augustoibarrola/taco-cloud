fa06b259339c84076c98ef654e6270c09dc4e09e
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-10-31 16:29:23 -0400

    finished ch-4.4-knowing-your-user

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -2,6 +2,8 @@ package tacos.controllers;
 
 import javax.validation.Valid;
 
+//import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.domain.Order;
+import tacos.domain.User;
 import tacos.repository.OrderRepository;
 
 @Slf4j
@@ -32,9 +35,16 @@ public class OrderController {
         }
     
     @PostMapping
-    public String processOrder(@Valid Order order, SessionStatus sessionStatus, Errors error) {
+    public String processOrder(
+            @Valid Order order, 
+            @AuthenticationPrincipal User user, 
+            SessionStatus sessionStatus, 
+            Errors error) 
+    {
         
         if(error.hasErrors()) return "orderForm";
+        
+        order.setUser(user);
         
         orderRepo.save(order);
 
