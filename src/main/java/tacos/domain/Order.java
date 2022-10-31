c32e4bacd7cdcadb0c1960fd45b5ec9556ca650e
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-10-31 16:27:17 -0400

    finished ch-4.4-knowing-your-user

--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
@@ -49,6 +50,8 @@ public class Order implements Serializable{
     private String ccCVV;
     @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
+    @ManyToOne
+    private User user;
     
     public void addTaco(Taco taco) {this.tacos.add(taco);}
     
