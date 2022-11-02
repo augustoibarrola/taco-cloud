0a8151349d8437e5109fb58d904701c24b098323
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-02 15:28:48 -0400

    misc

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -11,13 +11,6 @@ public class Ingredient {
 	private final String name;
 	private final Type type;
 	
-	public Ingredient(String id, String name, Type type) {
-		
-		this.id= id;
-		this.name = name;
-		this.type = type;
-		
-	}
 	
 	public static enum Type {
 		
@@ -25,10 +18,6 @@ public class Ingredient {
 		
 	}
 
-    public Object getType() {
-        // TODO Auto-generated method stub
-        return null;
-    }
 	
 	
 
