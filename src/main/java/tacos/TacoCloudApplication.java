745e76d9c0311fb6b00b58724d19fb42e96324af
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-03 14:59:54 -0400

    no code

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,7 +3,7 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Chapter 6: Creating REST services
+// Chapter 6.1: Writing RESTful Controllers
 
 @SpringBootApplication
 public class TacoCloudApplication {
