4a357c512f8df3abed9f3b03c7b9828d0bd49b97
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-03 14:54:14 -0400

    starting chapter 6

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,6 +3,7 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Chapter 6: Creating REST services
 
 @SpringBootApplication
 public class TacoCloudApplication {
